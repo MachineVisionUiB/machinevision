23618b82ebdb6b112fbbd99b479c413053735fc1
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2019-08-17 14:28:43 +0200

    Issue 18 Create content type page template suggestions

--- template.php
+++ template.php
@@ -4,3 +4,10 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+function machinevision_preprocess_page(&$variables) {
+  if (isset($variables['node'])) {
+    $node_type = $variables['node']->type;
+    $variables['theme_hook_suggestions'][] = $node_type;
+  }
+}
