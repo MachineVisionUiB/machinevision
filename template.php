941eca03c4c91437155b34baff0beb1c5bfc0961
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2019-08-17 15:29:46 +0200

    Issue 18 Remove page title from landing pages
    Not needed on these pages

--- template.php
+++ template.php
@@ -8,6 +8,6 @@
 function machinevision_preprocess_page(&$variables) {
   if (isset($variables['node'])) {
     $node_type = $variables['node']->type;
-    $variables['theme_hook_suggestions'][] = $node_type;
+    $variables['theme_hook_suggestions'][] = 'page__' . $node_type;
   }
 }
--- templates/page--landing-page.tpl.php
+++ templates/page--landing-page.tpl.php
@@ -141,9 +141,6 @@
       endif;?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
       <?php if (!empty($tabs)): ?>
